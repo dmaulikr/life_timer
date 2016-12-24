6765818fbe0bb55984e413fa61e0d064ea0f1548
Kvanland <kvanland@usc.edu>
2016-12-23 17:06:03 -0800

    Testing image view

--- Life Timer/Life Timer/FirstViewController.swift
+++ Life Timer/Life Timer/FirstViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Properties
+    @IBOutlet weak var timerNameTextField: UITextField!
+    @IBOutlet weak var timerNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    //Constructor
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Handles the text field's user input through delegate callbacks.
+        timerNameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UITextFieldDelegate
+    
+    //When the user presses the "return"/"done" button on the keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //hide the keyboard
+        timerNameTextField.resignFirstResponder()
+        return true //Returns whether the system should process the press of the "return" key
+    }
+    
+    //When the textfield resigns its FirstResponder status
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        timerNameLabel.text = timerNameTextField.text
+    }
+    
+    
+    
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        timerNameLabel.text = "Default Text"
+    }
+    
+    
+    
+    
+    
 }
 
